470e53b02ca605bbe06faefe1c25e2b6d3dc37d6
0Zeta <0Zeta@protonmail.com>
2020-04-05 18:17:31 +0200

    Tune hyperparameters

--- collab_and_comp/agent.py
+++ collab_and_comp/agent.py
@@ -9,14 +9,14 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e6)  # replay buffer size
+BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor
 LR_CRITIC = 1e-3        # learning rate of the critic
 LEARN_EVERY = 1         # learn every LEARN_EVERY steps
-LEARN_NB = 1            # how often to execute the learn-function each LEARN_EVERY steps
+LEARN_NB = 1            # how often to execute the learn-function every LEARN_EVERY steps
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
